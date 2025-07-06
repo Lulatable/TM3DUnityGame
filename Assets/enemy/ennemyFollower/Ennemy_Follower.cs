819ca9dc25b320646d9463e43064a7a1bce3386c
Lucie Rossé <lucie.rosse@cyberiacafe.ch>
2025-07-06 21:10:20 +0200

    Ennemies idle and scenes loading

--- Assets/enemy/ennemyFollower/Ennemy_Follower.cs
+++ Assets/enemy/ennemyFollower/Ennemy_Follower.cs
@@ -1,10 +1,12 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Rendering;
+
 
 public class Ennemy_Follower : MonoBehaviour
 {
@@ -13,21 +15,33 @@ public class Ennemy_Follower : MonoBehaviour
     public float distance;
 
     public Transform player;
+    public Transform ennemy;
     private NavMeshAgent agent;
     public Field_Of_View fov;
     public Field_Of_Attack foa;
     public Health healthManager;
     private long attackTick;
-
+    private Vector3 _targetPos;
+    private Vector3 _finalTargetPos;
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();       
+        agent = GetComponent<NavMeshAgent>();
+        _finalTargetPos = getRandompos();
     }
 
     // Update is called once per frame
     void Update()
     {;
+        if (!fov.canSeePlayer || !foa.canAttackPlayer) 
+        {
+
+            agent.destination = _finalTargetPos;
+            if ((ennemy.transform.position - _targetPos).sqrMagnitude > 0.01f) 
+            {
+                _finalTargetPos = getRandompos();
+            }
+        }
         if (fov.canSeePlayer)  
         {
              agent.destination = player.position;
@@ -40,5 +54,12 @@ public class Ennemy_Follower : MonoBehaviour
         }
 
        
+    }
+    public Vector3 getRandompos()
+    {
+        _targetPos = Random.insideUnitCircle * distance;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(_targetPos, out hit, distance, 1);
+        return hit.position;
     }
 }
---
+++ Assets/menumanager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menumanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject quit;
+    [SerializeField] private GameObject play;
+
+
+    void Start()
+    {
+        quit.GetComponent<Button>().onClick.AddListener(QuitApplication);
+        play.GetComponent<Button>().onClick.AddListener(PlayApplication);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (variableManager.instance.isGameOver == true)
+        {
+            SceneManager.LoadSceneAsync("end");
+        }
+
+    }
+    void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    void PlayApplication()
+    {
+        
+        SceneManager.LoadSceneAsync("constant");
+        SceneManager.LoadSceneAsync("SampleScene",LoadSceneMode.Additive);
+    }
+
+
+
+
+}
